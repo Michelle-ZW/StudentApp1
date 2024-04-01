d7968f8cfd804cc0f2cfb11bd71373cd8f031b4c
Zhenqiao Wang <zwang279@my.centennialcollege.ca>
2024-04-01 13:04:58 -0400

    added StudentApp1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import StudentForm from "./StudentForm";"./StudentForm"
+
+const App = () => (
+  <div>
+    
+    <StudentForm />
+   
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ src/StudentForm.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import "./App.css";
+
+export default function StudentForm() {
+    return (
+        <div className="container">
+            <div>
+                <h1>
+                    Student Form
+                </h1>
+                <form>
+                    <table >
+                        <tr>
+                            <th>Student Name: </th>
+                            <td> <input type="text" required></input></td>
+                        </tr>
+                        <tr>
+                            <th>Student Email: </th>
+                            <td><input type="text" required></input></td>
+                        </tr>
+                        <tr>
+                            <th>Phone Number: </th>
+                            <td><input type="text" required></input></td>
+                        </tr>
+
+                    </table>
+                    <button className="submitBtn" type="submit">Submit</button>
+                </form>
+            </div>
+
+        </div>
+    )
+}
